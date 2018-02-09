613ba0d47b46fa8b29b194d82ac4ada99913dd1a
Mark Thebault <mark.thebault@cloudreach.com>
2018-02-09 15:45:12 +0100

    Add autoscaling on workers still work to be done

--- terraform/spark-master.tf
+++ terraform/spark-master.tf
@@ -13,6 +13,7 @@ resource "aws_instance" "spark_master" {
   subnet_id = "${module.vpc.private_subnets[0]}"
   vpc_security_group_ids = ["${aws_security_group.spark_master.id}"]
   key_name = "${aws_key_pair.emr_kp.id}"
+  iam_instance_profile = "${aws_iam_instance_profile.spark_profile.id}"
 
   user_data = "${data.template_file.spark_master_user_data.rendered}"
 
@@ -44,4 +45,50 @@ resource "aws_security_group" "spark_master" {
     protocol        = "-1"
     cidr_blocks     = ["0.0.0.0/0"]
   }
+}
+
+
+resource "aws_iam_instance_profile" "spark_profile" {
+  role = "${aws_iam_role.spark_cluster_role.name}"
+}
+
+resource "aws_iam_role" "spark_cluster_role" {
+  name = "spark-cluster-role"
+  path = "/"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+               "Service": "ec2.amazonaws.com"
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "spark_cluster_policy" {
+  name = "spark-cluster-policy"
+  role = "${aws_iam_role.spark_cluster_role.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
 }
--- terraform/spark-workers.tf
+++ terraform/spark-workers.tf
@@ -1,3 +1,59 @@
+module "asg" {
+  source = "modules/autoscaling"
+
+  name = "spark_workers"
+
+  # Launch configuration
+  lc_name = "spark-workers-lc"
+
+  image_id        = "${lookup(var.ami_coreos, var.aws_region)}"
+  instance_type   = "${var.spark_worker_instance_type}"
+  user_data       = "${data.template_file.spark_worker_user_data.rendered}"
+  security_groups = ["${aws_security_group.spark_worker.id}"]
+  iam_instance_profile = "${aws_iam_instance_profile.spark_profile.id}"
+
+  ebs_block_device = [
+    {
+      device_name           = "/dev/xvdz"
+      volume_type           = "gp2"
+      volume_size           = "50"
+      delete_on_termination = true
+    },
+  ]
+
+  root_block_device = [
+    {
+      volume_size = "50"
+      volume_type = "gp2"
+    },
+  ]
+
+  # Auto scaling group
+  asg_name                  = "spark-workers-asg"
+  vpc_zone_identifier       = "${module.vpc.private_subnets}"
+  health_check_type         = "EC2"
+  min_size                  = 2
+  max_size                  = 10
+  desired_capacity          = 2
+  wait_for_capacity_timeout = 0
+
+  tags = [
+    {
+      key                 = "Environment"
+      value               = "${var.environment}"
+      propagate_at_launch = true
+    }
+  ]
+}
+
+
+
+
+
+
+
+
+
 data "template_file" "spark_worker_user_data" {
   template = "${file("${path.module}/spark-configuration/spark-worker-userdata.tmpl")}"
 
@@ -10,22 +66,22 @@ data "template_file" "spark_worker_user_data" {
 }
 
 
-resource "aws_instance" "spark_worker" {
-  count = 2
-  ami           = "${lookup(var.ami_coreos, var.aws_region)}"
-  instance_type = "${var.spark_worker_instance_type}"
-  subnet_id = "${module.vpc.private_subnets[0]}"
-  vpc_security_group_ids = ["${aws_security_group.spark_worker.id}"]
-  key_name = "${aws_key_pair.emr_kp.id}"
-
-  user_data = "${data.template_file.spark_worker_user_data.rendered}"
-
-  tags {
-    Name = "SparkWorker"
-    Terraform = "true"
-    Environment = "${var.environment}"
-  }
-}
+# resource "aws_instance" "spark_worker" {
+#   count = 2
+#   ami           = "${lookup(var.ami_coreos, var.aws_region)}"
+#   instance_type = "${var.spark_worker_instance_type}"
+#   subnet_id = "${module.vpc.private_subnets[0]}"
+#   vpc_security_group_ids = ["${aws_security_group.spark_worker.id}"]
+#   key_name = "${aws_key_pair.emr_kp.id}"
+#
+#   user_data = "${data.template_file.spark_worker_user_data.rendered}"
+#
+#   tags {
+#     Name = "SparkWorker"
+#     Terraform = "true"
+#     Environment = "${var.environment}"
+#   }
+# }
 
 
 
